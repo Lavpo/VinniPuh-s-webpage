82c48fc2d6bce9ee5038478380d795cac165bc0d
Lavpo <pavopavlo@gmail.com>
2026-04-06 20:31:45 +0300

    1.1.2 -- added some general look to updatelog section with borders, svgs, font-sizes and such

--- public/js/update-log-opener.js
+++ public/js/update-log-opener.js
@@ -4,18 +4,23 @@
 function LogOpener (){
     let lt = document.getElementById("log-top");
     let lb = document.getElementById("log-message");
+    let ul = document.getElementById("updatelog");
+    let ci = document.getElementById("imgChloe");
 
-    if (lb && lt){
+    if (lb && lt && ul && ci){
         if (getComputedStyle(lb).display === "block"){
             lb.style.display = "none";
             lt.style.borderRadius = "10px";
+            ul.style.border = "none";
+            lt.style.border = "solid 1px #741A2E";
+            ci.src = "images/icons/chenrons/chevron-down-svgrepo-com.svg";
         }
         else if(getComputedStyle(lb).display === "none"){
             lb.style.display = "block";
-            lt.style.borderBottomLeftRadius = "0px";
-            lt.style.borderBottomRightRadius = "0px";
-            lt.style.borderTopLeftRadius = "10px";
-            lt.style.borderTopRightRadius = "10px";
+            lt.style.borderRadius = "10px 10px 0 0";
+            ul.style.border = "solid 1px #741A2E";
+            lt.style.border = "none";
+            ci.src = "images/icons/chenrons/chevron-up-svgrepo-com.svg";
         }
     }
 }
