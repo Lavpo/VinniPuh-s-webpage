6f5cdbb18265a5f0116f255e62b141b81152aa99
Lavpo <pavopavlo@gmail.com>
2026-04-28 22:35:08 +0300

    1.4.5: implemented a decent mobile and tablet support by using media queries and did some minor bug fixes:
    -Added animation to the burger icon.
    -Added visuals to the top bar.
    -Adjusted width of the elements to be relative to what screen size is
    used. On mobiles, width has min and max sizes (exept for the Melia
    container) and on tablets width has only min and 100% set as default (again exept
    for the Melia container).

--- public/js/HamburgerClicker.js
+++ public/js/HamburgerClicker.js
@@ -2,5 +2,6 @@ const menu = document.querySelector(".Menu");
 const hambtn = document.getElementById("hambtn");
 
 hambtn.addEventListener("click", () => {
+  hambtn.classList.toggle("active");
   menu.classList.toggle("active");
 });
