1298de80ebbdcae9a4e86faf34110541b9115ea9
Lavpo <pavopavlo@gmail.com>
2026-05-10 23:47:50 +0300

    1.5.2 - update log fixes

--- public/js/update-log-opener.js
+++ public/js/update-log-opener.js
@@ -8,21 +8,26 @@ function LogOpener (){
     let ci = document.getElementById("chevron-up-down");
 
     if (lb && lt && ul && ci){
-        if (getComputedStyle(lb).height === "224px"){
-            lb.style.maxHeight = "0px";
-            lb.style.padding = "0";
-            lb.style.border = "0";
+        if (getComputedStyle(lb).visibility === "visible"){
+            // lb.style.maxHeight = "0px";
+            lb.style.visibility = "hidden";
+            lb.style.height = "0";
+            lb.style.padding = "0.313rem 0.939rem 0 0.626rem";
             lt.style.borderRadius = "10px";
+            lb.style.borderTop = "0";
             ul.style.border = "none";
             lt.style.border = "solid 1px #741A2E";
             ci.src = "images/icons/chevrons/chevron-down-svgrepo-com.svg";
 
             console.log("1width: " + lt.style.width + ", offsetwidth: " + lt.offsetWidth + ", boundingClient: " + lt.getBoundingClientRect().width);
         }
-        else if(getComputedStyle(lb).height === "0px"){
-            lb.style.maxHeight = "224px";
+        else if(getComputedStyle(lb).visibility === "hidden"){
+            // lb.style.maxHeight = "224px";
+            lb.style.visibility = "visible";
+            lb.style.height = "100%";
             lb.style.padding = "1.25rem 0.939rem 0 0.626rem";
-            lb.style.border = "1px solid #f5d5d5";
+            // lb.style.border = "1px solid #f5d5d5";
+            lb.style.border = "0";
             lb.style.borderTop = "solid 1px #741A2E";
             lt.style.borderRadius = "10px 10px 0 0";
             ul.style.border = "solid 1px #741A2E";
